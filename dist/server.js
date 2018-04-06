f42366591eee2c568cd41dd1ca29b4c3ce7d2fbb
Kostyantyn Didenko <kdidenko@gmail.com>
2018-04-06 20:00:53 +0300

    fixed pug templates issues with spacing

--- dist/server.js
+++ dist/server.js
@@ -1,7 +1,10 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+}
 Object.defineProperty(exports, "__esModule", { value: true });
-const errorhandler_1 = require("errorhandler");
-const app_1 = require("./app");
+const errorhandler_1 = __importDefault(require("errorhandler"));
+const app_1 = __importDefault(require("./app"));
 /**
  * Error Handler. Provides full stack - remove for production
  */
