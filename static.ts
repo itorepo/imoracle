9343fdeabb535466bf6ff2b1e8f0034d8979ea33
dicryptoid <dicryptoid@gmail.com>
2018-04-11 16:56:16 +0300

    added some statis assets for better html; also created simple assets managements script

---
+++ static.ts
@@ -0,0 +1,11 @@
+import * as shell from "shelljs";
+
+// first, remove old dist files if any
+shell.rm("-rf", "./dist");
+// next, create the 'dist' folder again
+shell.mkdir("-p", "./dist");
+// then, copy pug views to the dist
+shell.cp("-R", "./views", "./dist/");
+// finally, add public assets
+shell.cp("-R", "./src/public", "./dist/");
+
