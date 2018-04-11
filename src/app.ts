121e3d2cfba0d176196513d27b09ae2ecee463d9
dicryptoid <dicryptoid@gmail.com>
2018-04-11 16:16:59 +0300

    added home controller to handle the basic http request

--- src/app.ts
+++ src/app.ts
@@ -7,13 +7,20 @@ const app = express();
 
 
 // prepare server runtime parameters
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3001);
 // configure server view engine
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(
 	express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
 );
+
+
+// importing all project controllers
+import * as homeController from "./controllers/home"
+
+// defining routes handling points
+app.get("/", homeController.index);
 
 
 export default app;
---
+++ src/controllers/home.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+
+
+/**
+ * GET /
+ * Home page.
+ */
+export let index = (req: Request, res: Response) => {
+	res.render("home", {
+		title: "Home"
+	});
+};
+
